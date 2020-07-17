1c9c299a9cb21d685b2e7ce554fef8961e214fdf
James Addison <james@reciperadar.com>
2020-07-17 11:59:12 +0100

    Add naive endpoint implementation

--- web/app.py
+++ web/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, abort, jsonify, request
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
-def root():
-    products = request.args.getlist('products[]')
-    if not products:
-        return abort(400)
-
-    return jsonify(products)
+@app.route('/recipes/<recipe_id>')
+def recipe_diagnostics(recipe_id):
+    backend = 'http://backend-service'
+    recipe_crawled = requests.get(f'{backend}/recipes/{recipe_id}/crawl')
+    recipe_indexed = requests.get(f'{backend}/recipes/{recipe_id}')
+    recipe_history = requests.get(f'{backend}/recipes/{recipe_id}/history')
+    return jsonify({
+        'crawled': recipe_crawled.json(),
+        'indexed': recipe_indexed.json(),
+        'history': recipe_history.json(),
+    })
