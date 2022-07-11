4cea4a6dfa0c1447678792eb3911fdfe31895438
James Addison <james@reciperadar.com>
2022-07-11 12:49:13 +0100

    Implement basic test coverage on recipe retrieval endpoint

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,2 +1,19 @@
+import responses
+
+
+@responses.activate
 def test_request(client):
-    client.get("/recipes/example_id")
+    recipe_id = "example_id"
+    recipe = {"id": recipe_id}
+
+    responses.get(f"http://backend-service/recipes/{recipe_id}/crawl", json=recipe)
+    responses.get(f"http://backend-service/recipes/{recipe_id}", json=recipe)
+    responses.get(f"http://backend-service/recipes/{recipe_id}/history", json=recipe)
+
+    data = client.get(f"/recipes/{recipe_id}").json
+
+    assert data == {
+        "crawled": recipe,
+        "indexed": recipe,
+        "history": recipe,
+    }
